3bfb576cd1cde68f65cb8f4c529180aed0367e98
akancodes <akancodes@gmail.com>
2022-01-22 07:27:56 +0300

    second lesson finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect, Router } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
@@ -6,6 +6,9 @@ import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
